33d8f93b7a9a9769b99666676e5368e448663597
enneract9 <ottoblau@yandex-team.ru>
2023-11-14 23:06:57 +0500

    Added collection view property

---
+++ ImportanceEnum.swift
@@ -0,0 +1,14 @@
+//
+//  ImportanceEnum.swift
+//  project todolist
+//
+//  Created by @_@ on 12.11.2023.
+//
+
+import Foundation
+
+enum Importance: String {
+    case usual
+    case important
+    case unimportant
+}
---
+++ ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  project todolist
+//
+//  Created by @_@ on 12.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private lazy var collectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .blue
+        return collectionView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(collectionView)
+        setupConstraints()
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            // collectionView
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+    }
+}
